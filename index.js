d2468835e2fa7e4f235d4cdf6f70c0a5f58e0273
Daniel Henrique <henriqueevaldo@outlook.com>
2022-03-29 19:57:51 -0300

    refactor structure

--- index.js
+++
@@ -1,9 +0,0 @@
-var dosbox = new Dosbox({
-    id: "dosbox",
-    onload: function (dosbox) {   
-        dosbox.run("gzdoom-4-7-1-Windows-64bit.zip", "./gzdoom.exe");
-    },
-    onrun: function (dosbox, app) {
-      console.log("App '" + app + "' is runned");
-    }
-  });
